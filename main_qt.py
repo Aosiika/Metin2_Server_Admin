2d07d4ac8de726d86d9a6ab13b850bd01f2cce03
Tu Nombre <tu_correo@example.com>
2024-11-23 22:41:44 +0100

    Commit inicial

---
+++ main_qt.py
@@ -0,0 +1,10 @@
+# main_qt.py
+from PyQt5 import QtWidgets
+import sys
+from gui_qt import ServerAdminApp
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = ServerAdminApp()
+    main_window.show()
+    sys.exit(app.exec_())
