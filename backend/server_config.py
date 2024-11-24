b5bf0df2f52429a91b40f6f55a5ff624e1c1897b
Aosika <108371742+Aosiika@users.noreply.github.com>
2024-11-24 21:01:49 +0100

    Update server_config.py

--- backend/server_config.py
+++ backend/server_config.py
@@ -2,28 +2,35 @@
 import json
 import os
 
-# Configuración del servidor de Metin2
-CONFIG_FILE = os.path.join("config", "server_config.json")
+# Ruta al archivo de configuración del servidor de Metin2
+CONFIG_FILE = os.path.join(os.path.dirname(__file__), '..', 'config', 'server_config.json')
 
 def load_server_config():
+    """Carga la configuración del servidor desde el archivo de configuración."""
     if os.path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE, 'r') as f:
-            return json.load(f)
+        try:
+            with open(CONFIG_FILE, 'r') as f:
+                return json.load(f)
+        except Exception as e:
+            print(f"Error al cargar el archivo de configuración: {e}")
+            return None
     else:
+        # Si no existe el archivo de configuración, devuelve valores predeterminados
         return {
-            "host": "127.0.0.1",  # Dirección IP del servidor
-            "port": 3306,           # Puerto de conexión (por ejemplo, para MySQL)
-            "user": "admin",       # Usuario para la base de datos
-            "password": "password", # Contraseña de la base de datos
-            "database": "player",  # Base de datos del servidor de Metin2
-            "theme": "light",      # Tema de la aplicación, 'light' o 'dark'
-            "language": "es"       # Idioma de la aplicación, 'es' o 'en'
+            "host": "127.0.0.1",      # Dirección IP del servidor
+            "port": 3306,             # Puerto de conexión (ej. para MySQL)
+            "user": "admin",          # Usuario para la base de datos
+            "password": "password",   # Contraseña de la base de datos
+            "database": "player",     # Base de datos del servidor de Metin2
+            "theme": "light",         # Tema de la aplicación, 'light' o 'dark'
+            "language": "es"          # Idioma de la aplicación, 'es' o 'en'
         }
 
 def save_server_config(config):
-    with open(CONFIG_FILE, 'w') as f:
-        json.dump(config, f, indent=4)
+    """Guarda la configuración actual en el archivo de configuración."""
+    try:
+        with open(CONFIG_FILE, 'w') as f:
+            json.dump(config, f, indent=4)
+    except Exception as e:
+        print(f"Error al guardar el archivo de configuración: {e}")
 
-def save_version_config(config):
-    with open("server_config.json", "w") as f:
-        json.dump(config, f, indent=4)
